8b6298aacb2d6f07a77b645cf76eaae1c1d5c446
Lenovo <orvinoray@gmail.com>
2025-06-24 22:40:47 +0700

    update for navigation bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl font-bold text-biru-600">Welcome</Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link href="/movie/avengers">Avenger Movie</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+  "./App.{js,jsx,ts,tsx}",
+  "./app/**/*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+  "./src/**/*.{js,jsx,ts,tsx}",
+],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
-        primary: "#4F46E5", // Indigo 600
-        secondary: "#F59E0B", // Amber 500
-        accent: "#10B981", // Emerald 500
-        background: "#F3F4F6", // Gray 200
-        text: "#111827", // Gray 900
-        biru: {
-          100: "#EFF6FF", // Blue 100
-          200: "#DBEAFE", // Blue 200
-          300: "#BFDBFE", // Blue 300
-          400: "#93C5FD", // Blue 400
-          500: "#60A5FA", // Blue 500
-          600: "#3B82F6", // Blue 600
-          700: "#2563EB", // Blue 700
-          800: "#1D4ED8", // Blue 800
-          900: "#1E40AF", // Blue 900
-        }
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C7FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        accent: "#AB8BFF",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
